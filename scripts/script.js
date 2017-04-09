f815140c41193823d01bd7cca94d0a807a695180
ivailoAndonov <ivailoandonov.andonov@gmail.com>
2017-04-09 14:43:29 +0300

    fix click calls

--- scripts/script.js
+++ scripts/script.js
@@ -16,6 +16,27 @@ window.onscroll = debounceFunc.debounce(function () {
     }
 }, 50);
 
+var home = document.getElementsByClassName('home')[0];
+home.addEventListener('click', function () {
+    var toHome = document.getElementsByClassName('header-wrapper')[0];
+    smoothScroll.scroll(toHome);
+});
+var about = document.getElementsByClassName('about')[0];
+about.addEventListener('click', function () {
+    var toAbout = document.getElementsByClassName('about-me-wrapper')[0];
+    smoothScroll.scroll(toAbout);
+});
+var skills = document.getElementsByClassName('skills')[0];
+skills.addEventListener('click', function (){
+    var toSkills = document.getElementsByClassName('skills-wrapper')[0];
+    smoothScroll.scroll(toSkills);
+});
+var contacts = document.getElementsByClassName('contacts')[0];
+contacts.addEventListener('click', function (){
+    var toContacts = document.getElementsByClassName('contacts-wrapper')[0];
+    smoothScroll.scroll(toContacts);
+});
+
 var currentYear = new Date().getFullYear(),
     year = document.getElementsByClassName('year')[0];
 year.innerHTML = currentYear;
